007fd189aea620d8c09817dede8407ed70fcf63f
mohitjainonline <mohit.jainonline@gmail.com>
2018-03-01 14:51:21 +0530

    fix

--- client/views/breadcrumb.js
+++ client/views/breadcrumb.js
@@ -1,14 +1,14 @@
 import React from 'react';
  
 const breadcrumb = ({
-   
+   name
 }) =>(
     <div className="row">
           <div className="col-lg-12">
-            <h3 className="page-header"><i className="fa fa-laptop"></i> {this.props.name}</h3>
+            <h3 className="page-header"><i className="fa fa-laptop"></i> {name}</h3>
             <ol className="breadcrumb">
               <li><i className="fa fa-home"></i><a href="/">Home</a></li>
-              <li><i className="fa fa-laptop"></i>{this.props.name}</li>
+              <li><i className="fa fa-laptop"></i>{name}</li>
             </ol>
           </div>
         </div>

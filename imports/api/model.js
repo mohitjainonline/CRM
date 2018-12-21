10addb73c82489433aedd3b400cf7dc8b4212463
ETEC\mohitjain <mohitjain@policybazaar.com>
2018-12-21 16:47:00 +0530

    change

--- imports/api/model.js
+++ imports/api/model.js
@@ -1,7 +1,8 @@
 import { Mongo } from 'meteor/mongo'; 
 var maindb;
 if (Meteor.isServer) {
-    maindb = new MongoInternals.RemoteCollectionDriver("mongodb://mohitjain:abc123@ds153948.mlab.com:53948/dialercrm")
+    //maindb = new MongoInternals.RemoteCollectionDriver("mongodb://mohitjain:abc123@ds153948.mlab.com:53948/dialercrm")
+    maindb = new MongoInternals.RemoteCollectionDriver("mongodb://heroku_j4cdmcp8:9sdbr4mh4guvgo5d03pm02fpvc@ds141294.mlab.com:41294/heroku_j4cdmcp8")
 }
 
 export const CRMUsers = new Mongo.Collection('CRMUsers',{_driver : maindb});
